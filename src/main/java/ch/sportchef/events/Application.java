aa66f18b25c2a2ea52c2c9231b57f1e163cb1ed2
Marcus Fihlon <marcus@fihlon.ch>
2016-11-04 10:15:53 +0100

    Initial project setup

---
+++ src/main/java/ch/sportchef/events/Application.java
@@ -0,0 +1,43 @@
+/*
+ * SportChef – Sports Competition Management Software
+ * Copyright (C) 2016 Marcus Fihlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.sportchef.events;
+
+import ch.sportchef.events.business.ping.boundary.PingResource;
+import spark.Request;
+import spark.Response;
+
+import static spark.Spark.after;
+import static spark.Spark.port;
+
+public class Application {
+
+    public static void main(final String... args) {
+
+        // Configure Spark
+        port(8080);
+
+        // Register routes
+        new PingResource();
+
+        // Set up after-filters (called after each request)
+        after((Request request, Response response) -> {
+            response.header("Content-Encoding", "gzip");
+        });
+    }
+
+}
